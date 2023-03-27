342f0fbb8ee38d6ecb161b0c4ed444e1cd50f7cf
Zicka67 <art.book@hotmail.fr>
2023-03-27 16:47:02 +0200

    Comment TopicManager file

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -3,11 +3,14 @@
     
     use App\Manager;
     use App\DAO;
-    use Model\Managers\TopicManager;
+    // use Model\Managers\TopicManager;
 
+    //TopicManager hérite de la class Manager
     class TopicManager extends Manager{
 
+        //className = nom de votre classe du dossier "entities"
         protected $className = "Model\Entities\Topic";
+        //nom de la table en base de données
         protected $tableName = "topic";
 
 
