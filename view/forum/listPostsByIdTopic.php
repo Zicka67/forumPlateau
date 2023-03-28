81794c2dbf04e67abaf572fe3c25c761b1acb6e8
Zicka67 <art.book@hotmail.fr>
2023-03-28 17:46:55 +0200

    Create listPostsByIdTopics

---
+++ view/forum/listPostsByIdTopic.php
@@ -0,0 +1,38 @@
+<?php
+$posts = $result["data"]['posts'];
+$topic = $result["data"]['topic'];
+?>
+
+
+
+<h1>liste post dans le topic <?= $topic->getTitle() ?></h1>
+
+<?php
+if(!$posts){
+    echo "vide pour le moment";
+    
+} else { ?>
+    <table class="listTable">
+    <thead>
+        <tr>
+            <th>message</th>
+            <th>auteur</th>
+            <th>date</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach($posts as $post){
+        ?>
+        <tr>
+            <td><?= $post->getText() ?></td>
+            <td><?= $post->getUser() ?> </td>
+            <td><?= $post->getDatePost() ?></td>
+        </tr>      
+        <?php
+    } 
+    ?>
+    </tbody>
+    </table>
+    <?php
+} ?>
