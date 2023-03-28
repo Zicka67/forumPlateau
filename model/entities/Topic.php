b246f2ac1ac9e8ef9662c86f11d2d09785c4d564
Zicka67 <art.book@hotmail.fr>
2023-03-28 17:42:00 +0200

    Nothing

--- model/entities/Topic.php
+++ model/entities/Topic.php
@@ -1,127 +1,127 @@
 <?php
-    namespace Model\Entities;
+namespace Model\Entities;
 
-    use App\Entity;
+use App\Entity;
 
-    final class Topic extends Entity{
-
+final class Topic extends Entity{
+        
         private $id;
         private $title;
         private $creationDate;
         private $category;
         private $user;
         private $closed;
-
+        
         public function __construct($data){     
                 //hydrate pour remplir les propriétés de l'objet Topic avec les données en DB    
-            $this->hydrate($data);        
+                $this->hydrate($data);        
         }
- 
+        
         /**
-         * Get the value of id
-         */ 
+        * Get the value of id
+        */ 
         public function getId()
         {
                 return $this->id;
         }
-
+        
         /**
-         * Set the value of id
-         *
-         * @return  self
-         */ 
+        * Set the value of id
+        *
+        * @return  self
+        */ 
         public function setId($id)
         {
                 $this->id = $id;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of title
-         */ 
+        * Get the value of title
+        */ 
         public function getTitle()
         {
                 return $this->title;
         }
-
+        
         /**
-         * Set the value of title
-         *
-         * @return  self
-         */ 
+        * Set the value of title
+        *
+        * @return  self
+        */ 
         public function setTitle($title)
         {
                 $this->title = $title;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of user
-         */ 
+        * Get the value of user
+        */ 
         public function getUser()
         {
                 return $this->user;
         }
-
+        
         /**
-         * Set the value of user
-         *
-         * @return  self
-         */ 
+        * Set the value of user
+        *
+        * @return  self
+        */ 
         public function setUser($user)
         {
                 $this->user = $user;
-
+                
                 return $this;
         }
-
+        
         public function getCreationDate(){
-            $formattedDate = $this->creationDate->format("d/m/Y, H:i:s");
-            return $formattedDate;
+                $formattedDate = $this->creationDate->format("d/m/Y, H:i:s");
+                return $formattedDate;
         }
-
+        
         public function setCreationDate($date){
-            $this->creationDate = new \DateTime($date);
-            return $this;
+                $this->creationDate = new \DateTime($date);
+                return $this;
         }
-
+        
         /**
-         * Get the value of closed
-         */ 
+        * Get the value of closed
+        */ 
         public function getClosed()
         {
                 return $this->closed;
         }
-
+        
         /**
-         * Set the value of closed
-         *
-         * @return  self
-         */ 
+        * Set the value of closed
+        *
+        * @return  self
+        */ 
         public function setClosed($closed)
         {
                 $this->closed = $closed;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of category
-         */
+        * Get the value of category
+        */
         public function getCategory()
         {
                 return $this->category;
         }
-
+        
         /**
-         * Set the value of category
-         */
+        * Set the value of category
+        */
         public function setCategory($category): self
         {
                 $this->category = $category;
-
+                
                 return $this;
         }
-    }
+}
--- model/entities/User.php
+++ model/entities/User.php
@@ -1,127 +1,128 @@
 <?php
-    namespace Model\Entities;
+namespace Model\Entities;
 
-    use App\Entity;
+use App\Entity;
+use DateTime;
 
-    final class User extends Entity{
-
+final class User extends Entity{
+        
         private $id;
         private $pseudo;
         private $password;
         private $dateCreate;
         private $email;
         private $role;
-
+        
         public function __construct($data){     
                 //hydrate pour remplir les propriétés de l'objet Topic avec les données en DB    
-            $this->hydrate($data);        
+                $this->hydrate($data);        
         }
- 
+        
         /**
-         * Get the value of id
-         */
+        * Get the value of id
+        */
         public function getId()
         {
                 return $this->id;
         }
-
+        
         /**
-         * Set the value of id
-         */
+        * Set the value of id
+        */
         public function setId($id): self
         {
                 $this->id = $id;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of pseudo
-         */
+        * Get the value of pseudo
+        */
         public function getPseudo()
         {
                 return $this->pseudo;
         }
-
+        
         /**
-         * Set the value of pseudo
-         */
+        * Set the value of pseudo
+        */
         public function setPseudo($pseudo): self
         {
                 $this->pseudo = $pseudo;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of password
-         */
+        * Get the value of password
+        */
         public function getPassword()
         {
                 return $this->password;
         }
-
+        
         /**
-         * Set the value of password
-         */
+        * Set the value of password
+        */
         public function setPassword($password): self
         {
                 $this->password = $password;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of dateCreate
-         */
+        * Get the value of dateCreate
+        */
         public function getDateCreate()
         {
-                return $this->dateCreate;
+                $dateCreate =$this->dateCreate->format("d/m/Y, H:i:s");
+                return $dateCreate;
         }
-
+        
         /**
-         * Set the value of dateCreate
-         */
+        * Set the value of dateCreate
+        */
         public function setDateCreate($dateCreate): self
-        {
-                $this->dateCreate = $dateCreate;
-
+        {       //  \ pour utiliser la class native "DateTime"
+                $this->dateCreate = new \DateTime($dateCreate);
                 return $this;
         }
-
+        
         /**
-         * Get the value of email
-         */
+        * Get the value of email
+        */
         public function getEmail()
         {
                 return $this->email;
         }
-
+        
         /**
-         * Set the value of email
-         */
+        * Set the value of email
+        */
         public function setEmail($email): self
         {
                 $this->email = $email;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of role
-         */
+        * Get the value of role
+        */
         public function getRole()
         {
                 return $this->role;
         }
-
+        
         /**
-         * Set the value of role
-         */
+        * Set the value of role
+        */
         public function setRole($role): self
         {
                 $this->role = $role;
-
+                
                 return $this;
         }
-    }
+}
