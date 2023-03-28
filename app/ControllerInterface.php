1c3b891fd56c04a888879b97ce2c5a48168fbe86
Zicka67 <art.book@hotmail.fr>
2023-03-28 10:01:47 +0200

    Comments for a better understanding

--- app/ControllerInterface.php
+++ app/ControllerInterface.php
@@ -1,8 +1,14 @@
 <?php
 
+//Le ControllerInterface permet de définir un contract que les autres controleurs doivent respecter en gros.
+//Chaque contrôleur peut être appelé de manière uniforme par le routeur de l'application.
+//On s'assure que les contrôleurs peuvent être échangés sans risque d'erreur dans les autres parties du code qui les appellent.
+
     namespace App;
 
     interface ControllerInterface{
 
+        //cette interface spécifie une méthode index() que chaque contrôleur doit uiliser, 
+        // afin de garantir une cohérence et une structure dans la gestion des requêtes utilisateur.
         public function index();
     }
