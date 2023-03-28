695664b3dd5b4a4480237b73bdc486b79af9dbc6
Zicka67 <art.book@hotmail.fr>
2023-03-28 15:41:14 +0200

    Adding listCategory + listTopicsByCategory function

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -1,28 +1,67 @@
 <?php
 
-    namespace Controller;
+namespace Controller;
 
-    use App\Session;
-    use App\AbstractController;
-    use App\ControllerInterface;
-    use Model\Managers\TopicManager;
-    use Model\Managers\PostManager;
+use App\Session;
+use App\AbstractController;
+use App\ControllerInterface;
+
+use Model\Managers\TopicManager;
+use Model\Managers\CategoryManager;
+use Model\Managers\PostManager;
+use Model\Managers\UserManager;
+
+class ForumController extends AbstractController implements ControllerInterface
+{
     
-    class ForumController extends AbstractController implements ControllerInterface{
-
-        public function index(){
-            $topicManager = new TopicManager();
-            // $topicManager->findAll(); //on peut également ajouter une request SQL pour tirer par exemple
-            
-            return [
-                "view" => VIEW_DIR."forum/listTopics.php",
-                "data" => [
-                    "topics" => $topicManager->findAll(["creationDate", "DESC"])//on peut également ajouter une request SQL pour tirer par exemple
+    public function index()
+    {
+        $topicManager = new TopicManager();
+        // $topicManager->findAll(); //on peut également ajouter une request SQL pour tirer par exemple
+        
+        return [
+            "view" => VIEW_DIR."forum/listTopics.php",
+            "data" => [
+                "topics" => $topicManager->findAll(["creationDate", "DESC"])//on peut également ajouter une request SQL pour tirer par exemple
                 ]
             ];
-        
-        }
-
-        
-
+            
     }
+        
+        // Liste Categorie
+        public function listCategory() {
+            
+            // Link au Manager
+            $categoryManager = new CategoryManager();
+            
+            
+            return [
+                "view" => VIEW_DIR."forum/listCategory.php",
+                "data" => [
+                    "category" => $categoryManager->findAll(["label","ASC"]),
+                    ]
+                ];  
+    }
+            
+        // Liste topics par categorie
+        public function listTopicsByCategory() {
+                
+                // Link au Manager
+                $categoryManager = new CategoryManager();
+                $topicManager = new TopicManager();
+
+                
+                
+                return [
+                    "view" => VIEW_DIR."forum/listTopicsByCategory.php",
+                    "data" => [
+                        "category" => $categoryManager->findAll(["label","ASC"]),
+                        "topic" => $topicManager->findAll(["title","ASC"]),
+                        ]
+                    ];  
+    }
+
+                
+}
+    
+    
