c5c542e08218e1f84308f4be3d0d0465e5b04e80
Zicka67 <art.book@hotmail.fr>
2023-03-28 17:47:26 +0200

    Create listTopicsByIsCategory

---
+++ view/forum/listTopicsByIdCategory.php
@@ -0,0 +1,26 @@
+<?php
+$topics = $result["data"]["topics"];
+$category = $result["data"]["category"];
+?>
+
+<h1>liste topics <?=$category->getLabel()?></h1>
+<!-- On va chercher dans le construct getLabel avec la $category qui a accées aux data -->
+
+<?php
+if(!$topics){
+    echo "vide pour le moment";
+    
+} else {    
+    
+    foreach($topics as $topic ){
+        ?>
+        <a href="index.php?ctrl=forum&action=listPostsByIdTopic&id=<?=$topic->getId()?>"> <br>Title: -<?= $topic->getTitle() ?>   <?= $topic->getUser() ?>  <br></a>
+        <?php
+    }
+    
+    
+    
+    
+    
+    
+} ?>
