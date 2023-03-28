91be7e7545430b46e6bdd1c690ee95d1d3366bcf
Zicka67 <art.book@hotmail.fr>
2023-03-28 10:02:06 +0200

    Comments for a better understanding

--- model/entities/Post.php
+++ model/entities/Post.php
@@ -1,102 +1,103 @@
 <?php
-    namespace Model\Entities;
+namespace Model\Entities;
 
-    use App\Entity;
+use App\Entity;
 
-    final class Post extends Entity{
-
+//final pour les classes empêche l'héritage
+final class Post extends Entity{
+        
         private $id;
         private $text;
         private $datePost;
         private $user;
         private $topic;
-
+        
         public function __construct($data){     
-                //hydrate pour remplir les propriétés de l'objet Topic avec les données en DB    
-            $this->hydrate($data);        
+                //hydrate permet "d'alimenter" des attributs avec des valeurs. Cette function a un tab associatif ($data) comme argument
+                $this->hydrate($data);        
         }
-
+        
         public function getDatePost(){
-            $formattedDate = $this->datePost->format("d/m/Y, H:i:s");
-            return $formattedDate;
+                $formattedDate = $this->datePost->format("d/m/Y, H:i:s");
+                return $formattedDate;
         }
-
+        
         public function setDatePost($date){
-            $this->datePost = new \DateTime($date);
-            return $this;
+                $this->datePost = new \DateTime($date);
+                return $this;
         }
- 
+        
         /**
-         * Get the value of id
-         */ 
+        * Get the value of id
+        */ 
         public function getId()
         {
                 return $this->id;
         }
-
+        
         /**
-         * Set the value of id
-         *
-         * @return  self
-         */ 
+        * Set the value of id
+        *
+        * @return  self
+        */ 
         public function setId($id)
         {
                 $this->id = $id;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of text
-         */
+        * Get the value of text
+        */
         public function getText()
         {
                 return $this->text;
         }
-
+        
         /**
-         * Set the value of text
-         */
+        * Set the value of text
+        */
         public function setText($text): self
         {
                 $this->text = $text;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of user
-         */
+        * Get the value of user
+        */
         public function getUser()
         {
                 return $this->user;
         }
-
+        
         /**
-         * Set the value of user
-         */
+        * Set the value of user
+        */
         public function setUser($user): self
         {
                 $this->user = $user;
-
+                
                 return $this;
         }
-
+        
         /**
-         * Get the value of topic
-         */
+        * Get the value of topic
+        */
         public function getTopic()
         {
                 return $this->topic;
         }
-
+        
         /**
-         * Set the value of topic
-         */
+        * Set the value of topic
+        */
         public function setTopic($topic): self
         {
                 $this->topic = $topic;
-
+                
                 return $this;
         }
-    }
+}
