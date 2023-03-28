361cb42c15b02015695206ac33d888856997039c
Zicka67 <art.book@hotmail.fr>
2023-03-28 10:03:38 +0200

    Create + adding login.php

---
+++ view/security/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Login</title>
+
+</head>
+<body>
+
+<form method="POST" action="du genre ?ctrlsecurity action login?">
+
+  <div>
+    <label for="email">Email :</label>
+    <input type="email" id="email" name="email" required>
+  </div>
+  <div>
+    <label for="password">Password :</label>
+    <input type="password" id="password" name="password" required>
+  </div>
+  <div>
+    <input type="submit" value="Log in">
+  </div>
+
+</form>
+
+</body>
+</html>
