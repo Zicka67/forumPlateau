bc3831ebe3f2c03ee89751c02915a49823ea9e7f
Zicka67 <art.book@hotmail.fr>
2023-03-28 17:43:13 +0200

    Modification on postByTopic function, adding connect + ORDER BY

--- model/managers/PostManager.php
+++ model/managers/PostManager.php
@@ -1,35 +1,55 @@
 <?php
-    namespace Model\Managers;
-     //Les class managers gérent les opérations CRUD
+namespace Model\Managers;
+//Les class managers gérent les opérations CRUD
+
+use App\Manager;
+use App\DAO;
+
+class PostManager extends Manager{
     
-    use App\Manager;
-    use App\DAO;
-
-    class PostManager extends Manager{
-
-           //className = nom de votre classe du dossier "entities"
-           protected $className = "Model\Entities\Post";
-           //nom de la table en base de données
-           protected $tableName = "post";
-
-           public function __construct(){
-            //parent::connect(); ici parent = app -> DAO et appel la function connect() 
-            parent::connect();
-        }
-
-        public function findPostsByTopic($id) {
-
-            $sql = "SELECT *
-                    FROM ".$this->tableName." p
-                    WHERE p.topic_id = :id_topic
-                    ";
-
-                    //Si plusieurs lignes --> getMultipleResults
-                    //Si un seul objet --> getOneOrNullResult
-                    return $this->getMultipleResults(
-                        DAO::select($sql, ['id' =>$id]),
-                        $this->className
-                    );
-        }
-
+    //className = nom de votre classe du dossier "entities"
+    protected $className = "Model\Entities\Post";
+    //nom de la table en base de données
+    protected $tableName = "post";
+    
+    public function __construct(){
+        //parent::connect(); ici parent = app -> DAO et appel la function connect() 
+        parent::connect();
     }
+    
+    public function getPostsByTopic($id) {
+        parent::connect();
+        $sql = "
+        SELECT *
+        FROM ".$this->tableName." p
+        WHERE p.topic_id = :id
+        ORDER BY datePost ASC
+        ";
+        
+        //Si plusieurs lignes --> getMultipleResults
+        //Si un seul objet --> getOneOrNullResult
+        return $this->getMultipleResults(
+            DAO::select($sql, ['id' =>$id]),
+            $this->className
+        );
+    }
+    
+    // Selectionner post by topic id
+    public function getPostsByIdTopic($id) {
+        parent::connect();
+        $sql ="
+        SELECT * 
+        FROM ".$this->tableName." p
+        WHERE p.topic_id = :id
+        ORDER BY datePost ASC
+        ";
+        
+        return $this->getMultipleResults(
+            DAO::select($sql, ['id' => $id]), 
+            $this->className
+        );
+    }
+    
+    
+    
+}
