19280151709aacafda38ce23b1e188c99cdb5c0e
Zicka67 <art.book@hotmail.fr>
2023-03-28 10:40:14 +0200

    Changing register href on layout.php

--- view/layout.php
+++ view/layout.php
@@ -41,7 +41,7 @@
                         else{
                             ?>
                             <a href="./view/security/login.php">Connexion</a>
-                            <a href="/security/register.html">Inscription</a>
+                            <a href="./view/security/register.php">Inscription</a>
                             <a href="index.php?ctrl=forum&action=listTopics">la liste des topics</a>
                         <?php
                         }
