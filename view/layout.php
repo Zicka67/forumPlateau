ecbf911053f69e594f0bc4b645192f63b883524b
Zicka67 <art.book@hotmail.fr>
2023-03-29 23:53:24 +0200

    Modif on css link ( work like this )

--- view/layout.php
+++ view/layout.php
@@ -6,12 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-    <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
+    <link rel="stylesheet" href="public\css\style.css">
+    <!-- <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">-->
     <title>FORUM</title>
 </head>
 <body>
     <div id="wrapper"> 
-       
+  
         <div id="mainpage">
             <!-- c'est ici que les messages (erreur ou succès) s'affichent-->
             <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
@@ -21,11 +22,12 @@
                     <div id="nav-left">
                         <a href="index.php">Accueil</a>
                         <?php
+                        //Fait planter le cache pour le moment
                         if(App\Session::isAdmin()){
-                            ?>
-                            <a href="index.php?ctrl=home&action=users">Voir la liste des gens</a>
+                         ?>
+                           <a href="index.php?ctrl=home&action=users">Voir la liste des gens</a>
                           
-                            <?php
+                          <?php
                         }
                         ?>
                     </div>
