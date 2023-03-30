4551c03b57edbfb44653d5b25d294820733e90bf
zicka67 <art.book@hotmail.fr>
2023-03-30 14:30:44 +0200

    adding addTopic + french to english for function name

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -13,92 +13,116 @@ use Model\Managers\UserManager;
 
 class ForumController extends AbstractController implements ControllerInterface
 {
-    
+
     public function index()
     {
         $topicManager = new TopicManager();
         // $topicManager->findAll(); //on peut également ajouter une request SQL pour tirer par exemple
-        
+
         return [
-            "view" => VIEW_DIR."forum/listTopics.php",
+            "view" => VIEW_DIR . "forum/listTopics.php",
             "data" => [
-                "topics" => $topicManager->findAll(["creationDate", "DESC"])//on peut également ajouter une request SQL pour tirer par exemple
-                ]
-            ];
-            
+                "topics" => $topicManager->findAll(["creationDate", "DESC"]) //on peut également ajouter une request SQL pour tirer par exemple
+            ]
+        ];
+    }
+
+    // Liste Categorie
+    public function listCategories()
+    {
+
+        // Link au Manager
+        $categoryManager = new CategoryManager();
+
+
+        return [
+            "view" => VIEW_DIR . "forum/listCategories.php",
+            "data" => [
+                "categories" => $categoryManager->findAll(["label", "ASC"]),
+            ]
+        ];
+    }
+
+    // Liste topics par categorie
+    public function listTopicsByIdCategory($id)
+    {
+
+        // Link au Manager
+        $categoryManager = new CategoryManager();
+        $topicManager = new TopicManager();
+
+
+
+        return [
+            "view" => VIEW_DIR . "forum/listTopicsByIdCategory.php",
+            "data" => [
+                "category" => $categoryManager->findOneById($id),
+                "topics" => $topicManager->getTopicsByCategory($id),
+            ]
+        ];
+    }
+
+
+    // Liste post par topic
+    public function listPostsByIdTopic($id)
+    {
+
+        // variable qui relie au manager
+        $postManager = new PostManager();
+        $topicManager = new TopicManager();
+
+        // renvoie 
+        return [
+            "view" => VIEW_DIR . "forum/listPostsByIdTopic.php",
+            "data" => [
+                "posts" => $postManager->getPostsByIdTopic($id),
+                "topic" => $topicManager->findOneById($id)
+            ]
+        ];
+    }
+
+    // AJOUT D'UN LABEL
+    public function addCategory()
+    {
+
+        $label = filter_input(INPUT_POST, "label", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+
+        $categoryManager = new CategoryManager();
+
+        // si les valeurs existent
+        if ($label) {
+            $newLabel = ["label" => $label];
+            $categoryManager->add($newLabel);
+            // on redirige
+            $this->redirectTo("forum", "listCategories");
         }
-        
-        // Liste Categorie
-        public function listCategories() {
-            
-            // Link au Manager
-            $categoryManager = new CategoryManager();
-            
-            
-            return [
-                "view" => VIEW_DIR."forum/listCategories.php",
-                "data" => [
-                    "categories" => $categoryManager->findAll(["label","ASC"]),
-                    ]
-                ];  
-            }
-            
-            // Liste topics par categorie
-            public function listTopicsByIdCategory($id) {
-                
-                // Link au Manager
-                $categoryManager = new CategoryManager();
-                $topicManager = new TopicManager();
-                
-                
-                
-                return [
-                    "view" => VIEW_DIR."forum/listTopicsByIdCategory.php",
-                    "data" => [
-                        "category" => $categoryManager->findOneById($id),
-                        "topics" => $topicManager->getTopicsByCategory($id),
-                        ]
-                    ]; 
-                }
-                
-                
-                // Liste post par topic
-                public function listPostsByIdTopic($id) {
-                    
-                    // variable qui relie au manager
-                    $postManager = new PostManager();
-                    $topicManager = new TopicManager();
-                    
-                    // renvoie 
-                    return [
-                        "view" => VIEW_DIR."forum/listPostsByIdTopic.php",
-                        "data" => [
-                            "posts" => $postManager->getPostsByIdTopic($id),
-                            "topic" => $topicManager->findOneById($id)
-                            ]
-                        ];  
-                    }
-                    
-                    // Add un label
-                    public function ajoutCategory() {
-                        
-                        $label= filter_input(INPUT_POST, "label", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                        
-                        
-                        $categoryManager = new CategoryManager();
-                        
-                        // si les valeurs existent
-                        if($label) {
-                            $newLabel=["label"=>$label];
-                            $categoryManager->add($newLabel);
-                            // on redirige
-                            $this->redirectTo("forum","listCategories");
-                        }
-                    }
-                    
-                }                   
-                
-                
-                
-                
-                
+    }
+
+    // AJOUT D'UN TOPIC
+    public function addTopic($id)
+    {
+        // filtres pour la sécurité du formulaire
+        $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $text = filter_input(INPUT_POST, "text", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        // renvoi à un user ?
+
+        $userId = 1;
+        //variable qui relie au manager TOPIC
+        $topicManager = new TopicManager();
+        $postManager = new PostManager();
+
+        // si les valeurs existent
+        if ($title && $text) {
+            // $data déclarée pour être utilisée dans la fonction add($data) dans manager
+            $newTopic = ["title" => $title, "category_id" => $id, "user_id" => $userId];
+            // prend une fonction auto-intégré "lastinsertid"
+            $topicId = $topicManager->add($newTopic);
+
+            $newPost = ["text" => $text, "topic_id" => $topicId, "user_id" => $userId];
+            $postManager->add($newPost);
+
+            $this->redirectTo("forum", "listTopicsByIdCategory", $id);
+        }
+    }
+}
