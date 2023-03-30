dd17c26759383fe9885e74fef976e7a502065fe7
zicka67 <art.book@hotmail.fr>
2023-03-30 14:28:11 +0200

    Create editPost

---
+++ view/forum/editPost.php
@@ -0,0 +1,15 @@
+<?php
+$posts = ($result["data"]['post']);
+?>
+
+<h1>Modification du message</h1>
+
+<form method="POST">
+    <label>
+        modifier le texte :<br>
+        <textarea name="text">aa</textarea>
+        <textarea name="text"><?= $post->getText()?></textarea>
+    </label>
+    <br>
+    <input type="submit" value="Modifier">
+</form>
