2f9d8dfd9a43e27db7255e4f23bed68fc6e0de22
Zicka67 <art.book@hotmail.fr>
2023-04-01 23:21:45 +0200

    Rewrite addTopic deletTopic lockTopicById + UnlockTopicById ( for later )

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -1,40 +1,85 @@
 <?php
-    namespace Model\Managers;
-     //Les class managers gérent les opérations CRUD
+namespace Model\Managers;
+//Les class managers gérent les opérations CRUD
+
+use App\Manager;
+use App\DAO;
+// use Model\Managers\TopicManager;
+
+//TopicManager hérite de la class Manager
+class TopicManager extends Manager{
     
-    use App\Manager;
-    use App\DAO;
-    // use Model\Managers\TopicManager;
-
-    //TopicManager hérite de la class Manager
-    class TopicManager extends Manager{
-
-        //className = nom de votre classe du dossier "entities"
-        protected $className = "Model\Entities\Topic";
-        //nom de la table en base de données
-        protected $tableName = "topic";
-
-
-        public function __construct(){
-            //parent::connect(); ici parent = app -> DAO et appel la function connect() 
-            parent::connect();
-        }
-
-        // Liste topics par categorie
-        public function getTopicsByCategory($id) {
-            parent::connect();
-            $sql ="
-            SELECT * 
-                FROM ".$this->tableName." t
-                WHERE t.category_id = :id
-                ORDER BY creationDate DESC
-            ";
-            
-            return $this->getMultipleResults(
-                DAO::select($sql, ['id' => $id]), 
-                $this->className
-            );
-        }
-
-
+    //className = nom de votre classe du dossier "entities"
+    protected $className = "Model\Entities\Topic";
+    //nom de la table en base de données
+    protected $tableName = "topic";
+    
+    
+    public function __construct(){
+        //parent::connect(); ici parent = app -> DAO et appel la function connect() 
+        parent::connect();
     }
+    
+    // Liste topics par categorie
+    public function getTopicsByCategory($id) {
+        parent::connect();
+        $sql ="
+        SELECT * 
+        FROM ".$this->tableName." t
+        WHERE t.category_id = :id
+        ORDER BY creationDate DESC
+        ";
+        
+        return $this->getMultipleResults(
+            DAO::select($sql, ['id' => $id]), 
+            $this->className
+        );
+    }
+    
+    public function addTopic($id)
+    {
+        $sql="INSERT INTO topic (title)
+        VALUES (:title)";
+        
+        return $this-> getMultipleResults(
+            DAO::select($sql, ['id'=>$id], true),
+            $this->className
+        );
+    }
+    
+    public function deleteTopic($id)
+    {
+        $this->delete($id);
+    }
+    
+    public function getId(){
+        
+        
+    }
+    
+    public function lockTopicById($id)
+    {
+        parent::connect();
+        
+        $sql =  "UPDATE " . $this->tableName .
+        " SET closed = 1
+        WHERE id_topic = :id";
+        
+        
+        return DAO::update($sql, ['id' => $id]);
+    }
+    
+    public function unlockTopicById($id)
+    {
+        parent::connect();
+        
+        $sql =  "UPDATE " . $this->tableName .
+        " SET closed = 0 
+        WHERE id_topic = :id";
+        
+        
+        return DAO::update($sql, ['id' => $id]);
+    }
+    
+    
+}
