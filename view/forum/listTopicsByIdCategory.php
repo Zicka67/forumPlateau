86ce0acdd647ac45cd6867ff9d89da8d2354cbbf
Zicka67 <art.book@hotmail.fr>
2023-04-02 16:50:58 +0200

    veri admin session + css

--- view/forum/listTopicsByIdCategory.php
+++ view/forum/listTopicsByIdCategory.php
@@ -1,29 +1,25 @@
 <?php
 $topics = $result["data"]["topics"];
 $category = $result["data"]["category"];
-// var_dump($category); die;
 ?>
 
 <h1 class="categoryList">liste des topics <?=$category->getLabel()?></h1>
-            <div class="containerMain">
-                <a class="addTopicContainer" href="index.php?ctrl=Forum&action=addTopic&id=<?= $category->getId()?>">Ajouter un topic</a>
-            </div>
-<!-- On va chercher dans le construct getLabel avec la $category qui a accées aux data -->
-
-<?php
-if(!$topics){
-    echo "vide pour le moment";
-    
-} else {    
-    
-    foreach($topics as $topic ){
-        ?>
-        <div class="containerMain">
-            <div class="topicContainer">
-                <a href="index.php?ctrl=forum&action=listPostsByIdTopic&id=<?=$topic->getId()?>"><br><?= $topic->getTitle() ?> <span>crée le</span> <?= DateTime::createFromFormat('d/m/Y, H:i:s', $topic->getCreationDate())->format('d/m/Y') ?> <span>par</span>  <?=$topic->getUser() ?><br></a>
-            </div>
-        </div>
-        <?php
-    }
-    
-} ?>
+<?php  if(App\Session::isAdmin()) { ?>
+    <div class="containerMain">
+    <a class="addTopicContainer" href="index.php?ctrl=Forum&action=addTopic&id=<?= $category->getId()?>">Ajouter un topic</a>
+    </div>
+    <?php  }?>
+    <!-- On va chercher dans le construct getLabel avec la $category qui a accées aux data -->
+    <div>
+    <?php if(!$topics){ ?>
+        <div class="centerTopics">Vide pour le moment</div>
+        <?php } else { ?>
+            <?php foreach($topics as $topic){ ?>
+                <div class="containerMain">
+                    <div class="topicContainer">
+                        <a href="index.php?ctrl=forum&action=listPostsByIdTopic&id=<?=$topic->getId()?>"><br><?= $topic->getTitle() ?> <span>crée le</span> <?= DateTime::createFromFormat('d/m/Y, H:i:s', $topic->getCreationDate())->format('d/m/Y') ?> <span>par</span>  <?=$topic->getUser()->getPseudo() ?><br></a>
+                </div>
+                </div>
+                <?php } ?>
+                <?php } ?>
+    </div>
