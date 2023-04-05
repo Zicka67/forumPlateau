4e1b85572c8e737b9057a2e577b512f771463af4
Zicka67 <art.book@hotmail.fr>
2023-04-05 15:12:16 +0200

    Changement de class + ajout d'icon lock unlock +condition ( dosent work atm )

--- view/forum/listTopicsByIdCategory.php
+++ view/forum/listTopicsByIdCategory.php
@@ -7,43 +7,47 @@ $category = $result["data"]["category"];
 <?= $category->getLabel() ?>
 </h1>
 
-<div class="containerMain">
-<a class="addTopicContainer" href="index.php?ctrl=Forum&action=addTopic&id=<?= $category->getId() ?>">Ajouter un
-topic</a>
-</div>
 <!-- On va chercher dans le construct getLabel avec la $category qui a accées aux data -->
-<div>
 <?php if (!$topics) { ?>
+    
     
     <div class="centerTopics">Vide pour le moment</div>
     <?php } else { ?>
+        <div class="containerMainTopic">
+        <div class="containerMainAdd">
+        <a class="addTopicContainer" href="index.php?ctrl=Forum&action=addTopic&id=<?= $category->getId() ?>">Ajouter un
+        topic</a>
+        </div>
         <?php foreach ($topics as $topic) { ?>
-            <div class="containerMain">
             <div class="topicContainer">
+            
+            <div class="flexTopic">
             <a href="index.php?ctrl=forum&action=listPostsByIdTopic&id=<?= $topic->getId() ?>">
             <br>
             <?= $topic->getTitle() ?>
+            </div>
+            <div>
             <span>crée le</span>
             <?= DateTime::createFromFormat('d/m/Y, H:i:s', $topic->getCreationDate())->format('d/m/Y') ?>
             <span>par</span>
             <?= $topic->getUser()->getPseudo() ?>
             <!-- afficher le nombre de messages correspondant à chaque topic -->
             <span>(  <?= $topic->getCountPosts() ?> messages) </span>
-            <br>
+            </div>
             </a>
-            <?php if (App\Session::isAdmin()) { ?>
-                <a class="supprimerTopic"
-                href="index.php?ctrl=forum&action=deleteTopic&id=<?= $topic->getId() ?>">supprimer</a>
-                <?php } ?> 
-                <?php if(App\Session::isAdmin() || ($topic->getUser()->getId() == $_SESSION["user"]->getId())) { ?>
-                    <a class="" href="index.php?ctrl=forum&action=closeTopic&id=<?= $topic->getId() ?>">(De)Verrouillage</a>
-                    <?php } ?>
-                    
-                    </div>
-                    </div>
-                    <?php } ?>
-                    </div>
-                    </div>
-                    <?php } ?>
-                    
-                    </div>
+            
+            </div>
+            <div class="adminPannel">
+    <?php if (App\Session::isAdmin()) { ?>
+        <a class="supprimerTopic" href="index.php?ctrl=forum&action=deleteTopic&id=<?= $topic->getId() ?>">Supprimer</a>
+    <?php } ?> 
+    <?php if(App\Session::isAdmin() || ($topic->getUser()->getId() == $_SESSION["user"]->getId())) { ?>
+        <a class="" href="index.php?ctrl=forum&action=closeTopic&id=<?= $topic->getId() ?>">(De)Verrouillage</a>  
+        <i class="fa-solid <?= $topic->isClosed() ? 'fa-unlock green' : 'fa-lock red' ?>"></i>
+    <?php } ?>
+</div>
+
+        <?php } ?>
+    </div>
+<?php } ?>
+                            
