c4423a4e4a74307690cc4e6a90f911e2feb28bf9
Zicka67 <art.book@hotmail.fr>
2023-04-05 09:11:27 +0200

    Changement ( éviter l'utilisation de current() ) ajout de $result dans la $topic

--- view/forum/listPostsByIdTopic.php
+++ view/forum/listPostsByIdTopic.php
@@ -1,80 +1,68 @@
 <?php
 $posts = $result["data"]['posts'];
-$topic = $posts->current()->getTopic();
+$topic = $result["data"]['topic'];
+// var_dump($topic); die;
 $countPosts = 0;
 // var_dump($posts->current()->getTopic()->getCountPosts());die;
 
 ?>
 
 
-<h1 class="categoryList">La liste des messages dans le
-    <?= $topic->getTitle() ?>
-</h1>
+<h1 class="categoryList">La liste des messages dans le <?= $topic->getTitle() ?></h1>
 
 
 <?php
-
 if (!$posts) { ?>
-
     <div class="centerTopics">Vide pour le moment</div>
 <?php } else { ?>
     <div class="containerMain">
-
         <?php
         foreach ($posts as $post) {
+            // $countPosts = 1;
             $countPosts++;
-
-            ?>
-
-            <div class="postContainer">
-
-                <table class="listTable">
-                    <thead>
-                        <tr>
-                            <th>Message :</th>
-                            <?php if (App\Session::isAdmin()) { ?>
-                                <th><a class="supprimerPost"
-                                        href="index.php?ctrl=forum&action=deletePost&id=<?= $post->getId() ?>">Supprimer</a></th>
-                            <?php } ?>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <?= $post->getText() ?>
-                            </td>
-                        </tr>
-
-                    </tbody>
-
-                    <tr class="bottomInfos">
-                        <th class="small">
-                            <?= $post->getDatePost() ?>
-                        </th>
-                        <th class="small">par</th>
-                        <th class="small">
-                            <?= $post->getUser() ?>
-                        </th>
-                    </tr>
-
-                </table>
-            </div>
-            <?php
-        } ?>
-        <?php
-        echo "<p class='test'> Nombre de messages : " . $countPosts . "</p>";
+        //    echo $countPosts++;
+            // echo $post->getId()
         ?>
-        <div class="formPostRepondre">
-            <form method="POST" action="index.php?ctrl=forum&action=addPost">
-                <label class="postRepondre" for="">Envie de répondre ?
+            <div class="postContainer" >
+            <table class="listTable">
+                <thead>
+                    <tr>
+                        <th>Message :  <a class="supprimerPost" href="index.php?ctrl=forum&action=deletePost&id=<?= $post->getId() ?>">Supprimer</a></th>
+                                    
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?= $post->getText() ?></td>
+                    </tr>
+                <?php
+                     
+                ?>
+                </tbody>
+                <tr class="bottomInfos">              
+                    <th class="small"><?= $post->getDatePost() ?></th>
+                    <th class="small">par</th>
+                    <th class="small"><?= $post->getUser()?></th>
+                </tr>
+            </table>
+               <?php    
+               
+        }
+        
+        ?>  
+            </div>
+       <?php }  echo "<p class='testt'>Nombre de messages : " . $countPosts . "</p>"; ?>
+       <div class="formPostRepondre">
+                    <form method="POST" action="index.php?ctrl=forum&action=addPost">
+                        <label class="postRepondre" for="">Envie de répondre ?
                     <textarea name="message" cols="105" rows="6"></textarea>
                     <!-- hidden input pour stocker l'id de ce topic et l'envoyer dans addPost, surement mieux a faire -->
-                    <input type="hidden" name="topic_id" value="<?= $topic->getId() ?>">
+                    <input type="hidden" name="topic_id" value="<?= $topic->getId() ?>"> 
                     <input class="buton" type="submit" name="envoyer" value="Envoyer">
-                </label>
-            </form>
+                        </label>
+                    </form>
         </div>
     </div>
+<?php
 
-    <?php
-} ?>
+
